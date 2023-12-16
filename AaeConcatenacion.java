50c36ddff245fcaca620baa30f4fa83275e98ce4
ulisesGez <gu362937@uaeh.edu.mx>
2023-12-16 16:12:45 -0600

    concatenacion

--- AaeConcatenacion.java
+++ AaeConcatenacion.java
@@ -4,9 +4,18 @@ public class AaeConcatenacion {
          * Concatenación:
          * La concatenación es la unión de dos o más cadenas de texto.
          */
-        String nombre = "Juan";
+        var nombre = "Juan";
         String apellido = "Perez";
         String nombreCompleto = nombre + " " + apellido;
         System.out.println("Hola, " + nombreCompleto + "!");
+
+        var edad = 10;
+        var adulto = 18;
+        var faltantes = adulto - edad;
+
+        //Evalua de izquierda a derecha:
+        System.out.println(faltantes + edad + nombre);// 18Juan
+        System.out.println(nombre + faltantes + edad);// Juan810
+        System.out.println(nombre + (faltantes + edad));// Juan18
     }
 }
