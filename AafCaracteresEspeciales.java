4e945180356e87aa1d8ab14a0c48f5d30cebf2e2
ulisesGez <gu362937@uaeh.edu.mx>
2023-12-16 16:14:19 -0600

    caracteresEspeciales

---
+++ AafCaracteresEspeciales.java
@@ -0,0 +1,18 @@
+public class AafCaracteresEspeciales {
+
+    public static void main(String[] args) {
+        /*
+         * Caracteres especiales:
+         * Los caracteres especiales son aquellos que no se pueden escribir
+         * directamente en una cadena de texto.
+         * Para poder escribirlos, se debe anteponer una diagonal invertida (\)
+         * antes del caracter especial.
+         */
+        var nombre = "Juan";
+        System.out.println("Nueva linea: \n" + nombre);
+        System.out.println("Tabulador: \t" + nombre);
+        System.out.println("Retroceso: \b" + nombre);
+        System.out.println("Comilla simple: \'" + nombre + "\'");
+        System.out.println("Comilla doble: \"" + nombre + "\"");
+    }
+}
