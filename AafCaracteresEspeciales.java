fdb1514b980c0496ce00eb3e93818bfad5d00f84
ulisesGez <gu362937@uaeh.edu.mx>
2023-12-16 16:56:18 -0600

    caracteresEspeciales

--- AafCaracteresEspeciales.java
+++ AafCaracteresEspeciales.java
@@ -12,6 +12,7 @@ public class AafCaracteresEspeciales {
         System.out.println("Nueva linea: \n" + nombre);
         System.out.println("Tabulador: \t" + nombre);
         System.out.println("Retroceso: \b" + nombre);
+        System.out.println("Retroceso: \b\b" + nombre);
         System.out.println("Comilla simple: \'" + nombre + "\'");
         System.out.println("Comilla doble: \"" + nombre + "\"");
     }
