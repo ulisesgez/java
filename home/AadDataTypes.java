80b824229bb8d4406a308f22023ffb6f9b8df79e
ulisesGez <105885240+ulisesgez@users.noreply.github.com>
2024-03-30 20:14:51 -0600

    changes

--- home/AadDataTypes.java
+++ home/AadDataTypes.java
@@ -18,13 +18,13 @@ public class AadDataTypes {
             */
         System.out.println("Valor minimo de un byte: " + Byte.MIN_VALUE);
         System.out.println("Valor maximo de un byte: " + Byte.MAX_VALUE);
-        int variableInt = 10;
-        System.out.println("variable de tipo entero = " + variableInt);
-        String variableString = "Hola";
-        System.out.println("variable de tipo String = " + variableString);
         short variableShort = 10;
         System.out.println("variable de tipo short = " + variableShort);
         System.out.println("Valor minimo de un short: " + Short.MIN_VALUE);
         System.out.println("Valor maximo de un short: " + Short.MAX_VALUE);
+        int variableInt = 10;
+        System.out.println("variable de tipo entero = " + variableInt);
+        String variableString = "Hola";
+        System.out.println("variable de tipo String = " + variableString);
     }
 }
