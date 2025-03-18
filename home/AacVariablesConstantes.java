2894f1a6d14185f217cede4656ce0c6dd4ed8cd6
ulisesGez <105885240+ulisesgez@users.noreply.github.com>
2025-03-18 09:44:54 -0600

    text

--- home/AacVariablesConstantes.java
+++ home/AacVariablesConstantes.java
@@ -3,6 +3,10 @@ public class AacVariablesConstantes {
         /*
          * Variable:
          * Espacio de memoria que se reservan para almacenar datos.
+         *
+         * Constante:
+         * Valores que se mantienen siempre de igual manera y que no dependen
+         * de una asignacion para obtener e mismo.
          */
         int edad = 20;
         String nombre = "Juan";
