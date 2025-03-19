9dbc69b52c13d69dc0f54be21346335a5c9f71a8
ulisesGez <105885240+ulisesgez@users.noreply.github.com>
2025-03-19 10:01:54 -0600

    operadores

--- home/AaiOperadores.java
+++ home/AaiOperadores.java
@@ -1,5 +1,10 @@
 public class AaiOperadores {
     public static void main(String[] args) {
-        
+        /*
+        Operador:
+        Simbolo especial que indica al compilador que debe efectuar una determinada operacion.
+        Estas operaciones pueden ser de asignacion, aritmeticas, condicionales, relacionales,
+        entre otras.
+         */
     }
 }
