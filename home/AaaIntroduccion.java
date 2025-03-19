185141167a353825ce94e7346a774f60488cbdce
ulisesGez <105885240+ulisesgez@users.noreply.github.com>
2025-03-19 09:51:32 -0600

    introduccion

---
+++ home/AaaIntroduccion.java
@@ -0,0 +1,5 @@
+public class AaaIntroduccion {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
--- home/AacVariablesConstantes.java
+++ home/AacVariablesConstantes.java
@@ -14,7 +14,7 @@ public class AacVariablesConstantes {
         double averageGrade = 8.5;
         boolean isEnrolled = true;
         char gradeLevel = 'A';
-        
+
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
         System.out.println("ID: " + id);
