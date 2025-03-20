cc943d038a800ad91f3b108f036eabb114586136
ulisesGez <105885240+ulisesgez@users.noreply.github.com>
2025-03-20 10:37:15 -0600

    suma

--- home/AaiOperadores.java
+++ home/AaiOperadores.java
@@ -6,5 +6,9 @@ public class AaiOperadores {
         Estas operaciones pueden ser de asignacion, aritmeticas, condicionales, relacionales,
         entre otras.
          */
+        int number1, number2;
+        number1 = 10;
+        number2 = 20;
+        System.out.println(number1 + number2);
     }
 }
