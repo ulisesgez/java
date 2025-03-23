563d346eeefb45d3a21ca6ba7bd5b70a0433f76d
ulisesGez <105885240+ulisesgez@users.noreply.github.com>
2025-03-22 23:59:08 -0600

    new number

--- home/AaiOperadores.java
+++ home/AaiOperadores.java
@@ -6,10 +6,11 @@ public class AaiOperadores {
         Estas operaciones pueden ser de asignacion, aritmeticas, condicionales, relacionales,
         entre otras.
          */
-        double number1, number2;
+        double number1, number2, number3;
         number1 = 10;
         number2 = 20;
-        System.out.println("La suma de los dos numeros es:");
-        System.out.println(number1 + number2);
+        number3 = 70;
+        System.out.println("La suma de los tres numeros es:");
+        System.out.println(number1 + number2 + number3);
     }
 }
