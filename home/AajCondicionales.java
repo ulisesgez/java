73b383db12d1bf7e94eb28950297384d436fb27b
ulisesGez <105885240+ulisesgez@users.noreply.github.com>
2025-03-24 22:10:12 -0600

    condicinales

--- home/AajCondicionales.java
+++ home/AajCondicionales.java
@@ -2,7 +2,10 @@ public class AajCondicionales{
     public static void main(String[] args) {
             /*
             Condicionales:
-
+            Permiten que un programa ejecute diferentes acciooes segun ciertas
+            condiciones.
+            Son mecanismos que permiten que el programa siga distintos
+            caminos segun ciertos criterios.
              */
         System.out.println("Ingrese el primer numero");
         }
